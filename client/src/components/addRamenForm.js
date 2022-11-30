fc90d69bd425b3ef47d7dfa7b9866021b3e4e5e7
Ilia Zolas <ilia@thedmcsa.co.za>
2022-11-30 18:39:54 +0100

    ft/TOD-11 - onsubmit for add ramen

--- client/src/components/addRamenForm.js
+++ client/src/components/addRamenForm.js
@@ -1,22 +1,56 @@
+import { useState } from "react";
 
 const AddRamenForm = () => {
+    const [ramen, setRamen ] = useState([{
+        title:'',
+        ingredients:'',
+        description:''
+    }]);
+
+    const onSubmitForm = async e => {
+        try {
+            const body = { title, description, ingredients};
+            const response = await fetch("http://localhost:4000/ramen/new", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(body)
+            });
+      
+            window.location = "/ramen";
+          } catch (err) {
+            console.error(err.message);
+          }
+    };
+
  return (
-    <form>
+    <form method="post" onSubmit={onSubmitForm}>
         <label>
             Title
-            <input type="text" name="title" />
+            <input 
+                type="text" 
+                name="title" 
+                value="title"
+                onChange={e => setRamen(e.target.value.title)} />
         </label>
         <label>
             Ingredients
-            <input type="text" name="ingredients" />
+            <input 
+                type="text" 
+                name="ingredients" 
+                value="ingredients"
+                onChange={e => setRamen(e.target.value.ingredients)} />
         </label>
         <label>
             Description
-            <input type="text" name="description" />
+            <input 
+                type="text" 
+                name="description" 
+                value="description"
+                onChange={e => setRamen(e.target.value.description)} />
         </label>
         <input type="submit" value="Submit" />
     </form>
- )
+ );
 
 }
 
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -21,6 +21,6 @@ const Navbar = () => {
             </div>
         </div>
       );
-}
+};
 
 export default Navbar;
