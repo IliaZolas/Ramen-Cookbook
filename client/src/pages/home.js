3d12c60ce1e6b834938a5abb1ab13a60576c122a
Ilia Zolas <ilia@thedmcsa.co.za>
2023-01-08 11:16:28 +0100

    ft/todo-11 - created footer component, added image to hero banner from unsplash

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,14 +1,15 @@
 import {PrimaryButton} from "../components/buttons";
 import "./home.css"
+import Footer from "../components/footer";
 
 const Home = () => {
     return (
         <div className="">
             <div className="hero-banner">
                <h1>Ramen Cookbook</h1>
-               <h3>by Ilia Zolas</h3>
                <PrimaryButton />
             </div>
+            <Footer />
         </div>
       );
 }
